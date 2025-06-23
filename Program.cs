41b997a0258f259b838fb06895841742f22788b3
Danilo Gonçalves Pinheiro <124277720+DaniloPin@users.noreply.github.com>
2025-06-23 17:03:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿ContaBancaria conta = new ContaBancaria("34235", 1000.00);
+
+conta.Depositar(100.00);
+
+Console.WriteLine($"Conta: {conta.NumeroConta} ");
+Console.WriteLine($"Saldo atual: {conta.Saldo.ToString("F2")}");
+
